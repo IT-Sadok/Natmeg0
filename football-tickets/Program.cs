15f3b02fcb9bba4dbdad2947e2968250a5e79646
Natmeg0 <sv4519822@gmail.com>
2024-06-12 16:04:08 +0300

    Add files via upload

---
+++ football-tickets/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp8;
+
+Stadion stadion = new Stadion(10000);
+
+while (true)
+{
+    Console.WriteLine("Write the command you want to choose: Buy or Return a ticket");
+    string command = Console.ReadLine();
+
+    switch (command)
+    {
+        case "buy":
+            if (stadion.BuyTicket())
+            {
+                Console.WriteLine($"You bought a ticket. Tickets left:{stadion.GetAvailableTickets()}");
+            }
+            else
+            {
+                Console.WriteLine("You can't buy a ticket. Tickets are out");
+            }
+            break;
+
+        case "return":
+            if (stadion.ReturnTicket())
+            {
+                Console.WriteLine("You can't return a ticket.");
+            }
+            else
+            {
+                Console.WriteLine($"Ticket returned. Tickets left: {stadion.GetAvailableTickets()}");
+
+            }
+            break;
+    }
+}
---
+++ football-tickets/Stadion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Stadion
+    {
+        private int _maxSeats;
+        private int _reservedSeats;
+
+        public Stadion(int totalTickets)
+        {
+            _maxSeats = totalTickets;
+            _reservedSeats = 0;
+        }
+        public bool BuyTicket()
+        {
+            if (_reservedSeats < _maxSeats)
+            {
+                _reservedSeats += 1;
+                return true;
+            }
+            return false;
+        }
+
+        public bool ReturnTicket()
+        {
+            if (_reservedSeats > 0)
+            {
+                _reservedSeats -= 1;
+                return true;
+            }
+            return false;
+        }
+        public int GetAvailableTickets()
+        {
+            return _maxSeats - _reservedSeats;
+        }
+    }
+}
