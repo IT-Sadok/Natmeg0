c2187f9854ec855398a39cc1c318e43cfb5cb5c8
Natmeg0 <sv4519822@gmail.com>
2024-07-15 13:11:44 +0300

    Update Stadion.cs

--- tickets/Stadion.cs
+++ tickets/Stadion.cs
@@ -9,33 +9,64 @@ namespace ConsoleApp8
     public class Stadion
     {
         private int _maxSeats;
-        private List<Ticket> _tickets;
+        private Dictionary<Guid, Ticket> _tickets;
+        Queue<Guid> _returnQueue;
 
         public Stadion(int totalTickets)
         {
             _maxSeats = totalTickets;
-            _tickets = new List<Ticket>();
+            _tickets = new Dictionary<Guid, Ticket>();
+            _returnQueue = new Queue<Guid>();
         }
         public void BuyTicket()
         {
             if (_tickets.Count >= _maxSeats)
             {
-                throw new InvalidOperationException("Tickets are out");   
+                throw new InvalidOperationException("Tickets are out");
             }
-            _tickets.Add(new Ticket());    
+
+            Ticket newTicket = new Ticket();
+            _tickets.Add(newTicket.ID, newTicket);
         }
 
-        public void ReturnTicket()
+        public void RequestReturnTicket(Guid TicketID)
         {
-            if (_tickets.Count <= 0)
+            if (!_tickets.ContainsKey(TicketID))
             {
-                throw new InvalidOperationException("No tickets to return");     
+                throw new InvalidOperationException("Ticket with this ID not found");     
             }
-            _tickets.RemoveAt(0);
+            _returnQueue.Enqueue(TicketID);
+            Console.WriteLine($"A request to return a ticket with ID: {TicketID} has been added to the queue.");
+        }
+
+        public void ProcessReturnRequests()
+        {
+            while(_returnQueue.Count > 0)
+            {
+                Guid TicketID = _returnQueue.Dequeue();
+                _tickets.Remove(TicketID);
+            }
         }
         public int GetAvailableTickets()
         {
             return _maxSeats - _tickets.Count;
+        }
+
+        public List<Ticket> SearchTicketByDateRange(DateTime startDate, DateTime endDate)
+        {
+            var ticketsInRange = (from Ticket in _tickets.Values
+                                  where Ticket.DateCreated >= startDate && Ticket.DateCreated <= endDate
+                                  select Ticket).ToList();
+
+            return ticketsInRange;
+        }
+
+        public Ticket? GetRecentTicket()
+        {
+            var recentTicket = (from Ticket in _tickets.Values
+                                orderby Ticket.DateCreated descending
+                                select Ticket).FirstOrDefault();
+            return recentTicket;
         }
     }
 }
